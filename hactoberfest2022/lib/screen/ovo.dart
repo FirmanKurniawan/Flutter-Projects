6519cd665ba0a10428aad4dd27510d5f42b20c6e
Herry Prasetyo <herry081288@gmail.com>
2022-10-08 09:43:53 +0700

    last

--- hactoberfest2022/lib/screen/ovo.dart
+++ hactoberfest2022/lib/screen/ovo.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 
-class OvoUI extends StatelessWidget {
-  const OvoUI ({super.key});
+class OvoUI extends StatefulWidget {
+  const OvoUI({super.key});
+
+  @override
+  State<OvoUI> createState() => _OvoUIState();
+}
+
+class _OvoUIState extends State<OvoUI> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: PageView(
+        controller: _pageController,
+        children: [
+          Text('Hello Ovo'),
+        ],
+        physics: NeverScrollableScrollPhysics(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+      ),
+    );
   }
 }

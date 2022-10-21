66646302bc88019746e5f96c492f8d6ae8dcb12d
Syed Mehtab Ali <syedmehtab2125@gmail.com>
2022-10-22 00:04:01 +0500

    Migrate to the updated version of flutter

--- TileGame_by_insfirred/lib/resultwon.dart
+++ TileGame_by_insfirred/lib/resultwon.dart
@@ -1,49 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:tile_game/tileGame.dart';
 
-class popupWon extends StatefulWidget {
-  const popupWon({ Key? key }) : super(key: key);
+class PopupWon extends StatefulWidget {
+  const PopupWon({Key? key}) : super(key: key);
 
   @override
-  State<popupWon> createState() => _popupWonState();
+  State<PopupWon> createState() => _PopupWonState();
 }
 
-class _popupWonState extends State<popupWon> {
+class _PopupWonState extends State<PopupWon> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[900],
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Container(
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
             width: double.infinity,
             height: 500,
             color: Colors.grey[900],
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text('Yayy  :)  You found 7',textAlign: TextAlign.center,style: TextStyle(color: Colors.white ,fontSize: 40),),
+                const Text(
+                  'Yayy  :)  You found 7',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 40),
+                ),
                 FloatingActionButton(
                   backgroundColor: Colors.grey[800],
-                  onPressed: (){
+                  onPressed: () {
                     setState(() {
                       startOver();
                     });
                     Navigator.pop(context);
                   },
-                  child: Text('Play Again',textAlign: TextAlign.center,),
+                  child: const Text(
+                    'Play Again',
+                    textAlign: TextAlign.center,
+                  ),
                 )
-        
               ],
             ),
           ),
-          SizedBox(height: 50,)
-          ]
-        ),
+          const SizedBox(
+            height: 50,
+          )
+        ]),
       ),
     );
-
   }
 }

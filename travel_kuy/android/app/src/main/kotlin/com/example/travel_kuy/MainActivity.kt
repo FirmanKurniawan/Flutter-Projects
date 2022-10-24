b9b78a5cfbdb9c50250ebfd3a5ef7e06df12f1e1
Fadhila36 <yuffa36@gmail.com>
2022-10-24 17:21:18 +0700

    Add Travel-Kuy

---
+++ travel_kuy/android/app/src/main/kotlin/com/example/travel_kuy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travel_kuy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ travel_kuy/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:travel_kuy/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
